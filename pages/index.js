a4a66dadb8c985b7f961c3608137a2a89f67cad9
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-01-14 00:49:36 -0500

    37

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Head from "next/head";
+import { useState } from "react";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+  export default function Characters({characters}) {
+  const [value, setValue] = useState(12);
 
-export default function Home() {
-  return <Title>My page</Title>
+  const handleShowMore = () => {
+    setValue(value + 12);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Star Wars Characters | Starwars.com</title>
+      </Head>
+      <HomeScreenContainer>
+        <StarfieldLeft />
+        <CharactersContainer>
+          {characters
+            .filter((character) => character.id !== 28 && character.id !== 77)
+            .map(
+              (character, index) =>
+                index < value && (
+                  <Character character={character} key={character.id} />
+                )
+            )}
+        </CharactersContainer>
+        <StarfieldRight />
+      </HomeScreenContainer>
+    </>
+  );
 }
+
+export async function getStaticProps(context) {
+  const characters = await fetch(
+    "https://akabab.github.io/starwars-api/api/all.json"
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      characters,
+    },
+  };
+}
+
+const HomeScreenContainer = styled.div``;
+const StarfieldLeft = styled.div``;
+const StarfieldRight = styled.div``;
+const CharactersContainer = styled.div``;
