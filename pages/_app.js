c177d17746f69f095ec267ffae6cc56f7e813361
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-01-15 01:13:01 -0500

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #151515;
+  font-family: "DINNextW01-Regular", -apple-system, BlinkMacSystemFont, Segoe UI,
+    Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   }
 `;
 
@@ -18,8 +30,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-
+      <Header />
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import Head from "next/head";
 import { useState } from "react";
+import Character from "../components/Character";
 
-  export default function Characters({characters}) {
+export default function Characters({ characters }) {
   const [value, setValue] = useState(12);
 
   const handleShowMore = () => {
@@ -26,6 +27,9 @@ import { useState } from "react";
                 )
             )}
         </CharactersContainer>
+        {value < characters.length && (
+          <Button onClick={handleShowMore}>Show More</Button>
+        )}
         <StarfieldRight />
       </HomeScreenContainer>
     </>
@@ -45,6 +49,71 @@ export async function getStaticProps(context) {
 }
 
 const HomeScreenContainer = styled.div``;
-const StarfieldLeft = styled.div``;
-const StarfieldRight = styled.div``;
-const CharactersContainer = styled.div``;
+
+const CharactersContainer = styled.div`
+  display: flex;
+  margin-top: 200px;
+  padding: 30px;
+  justify-content: center;
+  gap: 25px;
+  flex-wrap: wrap;
+  background-color: #151515;
+`;
+
+const Starfield = styled.div`
+  position: fixed;
+  width: 200px;
+  top: 0;
+  background-repeat: repeat-y;
+`;
+
+// Extending styles
+const StarfieldLeft = styled(Starfield)`
+  left: 0;
+  height: 1700px;
+  background-position: left center;
+  background-size: 100% auto;
+  background-image: url(https://static-mh.content.disney.io/starwars/assets/background/bg_starsL-fd4661a3ccea.jpg);
+`;
+
+const StarfieldRight = styled(Starfield)`
+  right: 0;
+  height: 1700px;
+  background-position: right center;
+  background-size: auto 100%;
+  background-image: url(https://static-mh.content.disney.io/starwars/assets/background/bg_starsR-655c85e404d4.jpg);
+`;
+
+const Button = styled.button`
+  color: #aaa;
+  background-color: transparent;
+  border: none;
+  font-family: inherit;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  font-size: 18.4px;
+  display: block;
+  transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  :hover {
+    color: #fff;
+    cursor: pointer;
+  }
+  :hover::after {
+    border-bottom-color: #fff;
+    width: 100%;
+  }
+  ::after {
+    content: "";
+    display: block;
+    width: 40%;
+    margin: 0 auto;
+    opacity: 0.9;
+    border-bottom: 1px solid transparent;
+    /* Animations */
+    -webkit-transition: border-color 200ms, width 250ms;
+    -moz-transition: border-color 200ms, width 250ms;
+    transition: border-color 200ms, width 250ms;
+  }
+`;
